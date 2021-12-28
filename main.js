6af9b2ce000f4195e2566f18d71160429997dddc
OpAarush2941 <86848699+OpAarush2941@users.noreply.github.com>
2021-12-28 09:20:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas =  createCanvas(450,400);
+    canvas.center();
+    video = createCapture(400,400);
+    video.hide();
+    video.size(450,400);
+}
+
+function draw(){
+    image(video,0,0,450,400)
+}
